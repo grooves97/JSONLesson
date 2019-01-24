3dc4419a9f1c238b9ed6b640bc8b9743fc471c35
grooves97 <45422778+grooves97@users.noreply.github.com>
2019-01-25 00:56:57 +0600

    Testing
    Today i`m testing json format

---
+++ JSONLesson/JSONLesson/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+
+
+namespace JSONLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pearson = File.Exists("pearson.json") ? JsonConvert.DeserializeObject<Pearson>(File.ReadAllText("pearson.json")) : new Pearson
+            {
+                age = 19,
+                firstName = "Rassul",
+                lastName = "Gamzatov"
+            };
+
+            pearson.age++;
+
+            
+            File.WriteAllText("pearson.json", JsonConvert.SerializeObject(pearson));
+
+            //почему идет сериализация в строке 24 
+
+            //string jsonData = JsonConvert.SerializeObject(pearson);
+
+            //var pearson2 = JsonConvert.DeserializeObject<Pearson>(jsonData);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
